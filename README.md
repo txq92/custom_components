7d124994e460033d3b2e3da6f5206a29fcb55614
txq92 <56432960+txq92@users.noreply.github.com>
2024-04-21 10:33:41 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,30 +1,84 @@
-# custom_components
-hướng dẫn chi tiết tại :
-https://vnhass.blogspot.com/
+# Homeassistant
+# custom_components lịch âm
+Tải thư mục lịch âm và chép vào thư mục custom_components trong hass
 
-# nct_evncpc
-```
-https://vnhass.blogspot.com/2021/08/ha-evncpc-lay-chi-so-o-ien-tu-cong-to.html
-/config/custom_components/nct_evncpc/
-----
+
+Thêm trong configuration:
+
+```sh
 sensor:
-  - platform: nct_evncpc
-    name: '09095632xx'
-    passwd: 'passs'
+  - platform: lich_am
 
 ```
 
-# nct_evnhcm
-```
-https://vnhass.blogspot.com/2020/11/ha-evnhcm-lay-thong-tin-ien-luc-hcm-cho.html
-/config/custom_components/nct_evnhcm/
-----
-sensor:
-  - platform: nct_evnhcm
-    name: '0913661234'
-    passwd: 'passofyou'
-    makhach: 'PE120001XXXXX'
+Thêm trong automation
+```sh
+automation:
+  # Auto Nhắc rằm và mùng 1 qua ViPi vào 6h sáng và 18h tối vào trước 1 ngày và ngày rằm + mùng 1
+  - id: '0001'
+    alias: Nhắc rằm mùng 1
+    trigger:
+      - platform: time
+        at: '06:00:00'
+      - platform: time
+        at: '18:00.:00'
+    action:
+      - service: script.phat_loa_vipi
+        data_template:
+          message: >           
+            {% set ngay_mai = states('sensor.am_lich_ngay_mai') %}
+            {% set ngay_mai_int = ngay_mai.split('/')[0] | int %}
+            {% set ngay_hom_nay = states('sensor.am_lich_hom_nay') %}
+            {% set ngay_hom_nay_int = ngay_hom_nay.split('/')[0] | int %}
+            {% if ngay_mai_int == 1 %}
+              "Bản tin thông báo: Ngày mai là mùng 1 âm lịch. Nhằm {{ states('sensor.ngay_am_ngay_mai') }}."
+              "giờ tốt {{ states('sensor.gio_tot_ngay_mai') }} giờ xấu là giờ: {{ states('sensor.gio_xau_ngay_mai') }}."
+            {% elif ngay_mai_int == 15 %}
+              "Bản tin thông báo: Ngày mai là rằm. Nhằm {{ states('sensor.ngay_am_ngay_mai') }}."
+              " ngày mai {{ states('sensor.gio_tot_ngay_mai') }} giờ xấu là giờ:{{ states('sensor.gio_xau_ngay_mai') }}."
+            {% elif ngay_hom_nay_int == 15 %}
+              "Bản tin thông báo: Hôm nay là rằm. Nhằm  {{ states('sensor.ngay_am_hom_nay') }}."
+              "giờ tốt là giờ: {{ states('sensor.gio_tot_hom_nay') }} giờ xấu là giờ: {{ states('sensor.gio_xau_hom_nay') }}."
+            {% elif ngay_hom_nay_int == 1 %}
+              "Bản tin thông báo: Hôm nay là mùng 1. Nhằm {{ states('sensor.ngay_am_hom_nay') }}."
+              "giờ tốt là giờ: {{ states('sensor.gio_tot_hom_nay') }} giờ xấu là giờ: {{ states('sensor.gio_xau_hom_nay') }}."
+            {% endif %}  
 
-```
-Email : trumxuquang@gmail.com
+   ```
+# custom_components edge_tts
+Tải thư mục edge_tts và chép vào thư mục custom_components trong hass
 
+
+Thêm trong configuration:
+```sh
+tts:
+  - platform: edge_tts
+    service_name: edge
+    language: vi-VN-HoaiMyNeural
+    volume: +10%
+    rate: -10%
+
+   ```
+Ví dụ về scrip phát giọng Nữ edge-tts qua loa google
+```sh
+  phat_loa_phong_khach:
+    alias: "script phat loa phòng khách"
+    sequence:
+      - service: media_player.volume_set
+        target:
+          entity_id: media_player.phong_khach_2
+        data_template:
+          volume_level: "{{ volume | default(0.5) }}"  # Sử dụng giá trị mặc định là 0.5 nếu không có giá trị được cung cấp
+      - service: tts.edge
+        data_template:
+          entity_id: media_player.phong_khach_2
+          message: "{{ message }}"
+
+  test_script_media_pk:
+    alias: Test script phát media player phòng khách
+    sequence:
+      - service: script.phat_loa_phong_khach
+        data:
+          message: 'đây là scrip test loa media player phòng khách'
+          volume: 0.5  # Thay 0.5 bằng mức âm lượng mong muốn (giữa 0 và 1)
+   ```
---
+++ lich_am/sensor.py
@@ -0,0 +1,514 @@
+# encoding: utf-8
+'''
+=== Lunar calendar on Home Assistant by exlab ===
+============ Date 11/03/2019 ====================
+=== Mod by thangnd85
+--------
+#1 Copy sensor.py to {your_config folder}\custom_components\lich_am\
+#2 Setup in configuration.yaml 
+sensor:
+  - platform: lich_am
+    delimiter: "-"#Optional, default = "/"
+	type: number or text
+--------        
+'''
+import datetime
+import logging
+import math
+from datetime import timedelta
+import voluptuous as vol
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import CONF_SCAN_INTERVAL
+from homeassistant.util import Throttle
+from homeassistant.helpers.entity import Entity
+import homeassistant.helpers.config_validation as cv
+
+_LOGGER = logging.getLogger(__name__)
+
+CONF_DELIMITER = 'delimiter' 
+SCAN_INTERVAL = datetime.timedelta(seconds=300)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_DELIMITER, default= '/'): cv.string,
+})
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    delimiter = config.get(CONF_DELIMITER)
+    add_devices([lunar_calendar(delimiter),lunar_calendar_text(delimiter),lunar_calendar_good_hour_today(delimiter),lunar_calendar_bad_hour_today(delimiter),lunar_calendar_good_hour_tomorrow(delimiter),lunar_calendar_bad_hour_tomorrow(delimiter),lunar_calendar_nextday(delimiter),lunar_calendar_nextdate(delimiter)])
+
+class lunar_calendar(Entity):
+    def __init__(self, delimiter):
+        self._name = 'Âm lịch hôm nay'
+        self._state = None
+        self._delimiter = delimiter
+        self._description = 'Âm lịch @ Hồ Ngọc Đức'
+        self.update()
+    @property
+    def name(self):
+        return self._name
+    @property
+    def state(self):
+        return self._state
+    @property
+    def icon(self):
+        """Return the icon of the sensor"""
+        return 'mdi:calendar'
+    @property
+    def device_state_attributes(self):
+        return {'Thông tin': self._description}
+    @Throttle(SCAN_INTERVAL)
+    def update(self):
+        self._state = solar2lunar(self._delimiter)[0]
+class lunar_calendar_text(Entity):
+    def __init__(self, delimiter):
+        self._name = 'Ngày âm hôm nay'
+        self._state = None
+        self._delimiter = delimiter
+        self._description = 'Âm lịch @ Hồ Ngọc Đức'
+        self.update()
+    @property
+    def name(self):
+        return self._name
+    @property
+    def state(self):
+        return self._state
+    @property
+    def icon(self):
+        """Return the icon of the sensor"""
+        return 'mdi:calendar'
+    @property
+    def device_state_attributes(self):
+        return {'Thông tin': self._description}
+    @Throttle(SCAN_INTERVAL)
+    def update(self):
+        self._state = solar2lunar(self._delimiter)[1]
+        
+class lunar_calendar_good_hour_today(Entity):
+    def __init__(self, delimiter):
+        self._name = 'Giờ tốt hôm nay'
+        self._state = None
+        self._delimiter = delimiter
+        self._description = 'Âm lịch @ Hồ Ngọc Đức'
+        self.update()
+    @property
+    def name(self):
+        return self._name
+    @property
+    def state(self):
+        return self._state
+    @property
+    def icon(self):
+        """Return the icon of the sensor"""
+        return 'mdi:calendar'
+    @property
+    def device_state_attributes(self):
+        return {'Thông tin': self._description}
+    @Throttle(SCAN_INTERVAL)
+    def update(self):
+        self._state = solar2lunar(self._delimiter)[4]
+        
+class lunar_calendar_bad_hour_today(Entity):
+    def __init__(self, delimiter):
+        self._name = 'Giờ xấu hôm nay'
+        self._state = None
+        self._delimiter = delimiter
+        self._description = 'Âm lịch @ Hồ Ngọc Đức'
+        self.update()
+    @property
+    def name(self):
+        return self._name
+    @property
+    def state(self):
+        return self._state
+    @property
+    def icon(self):
+        """Return the icon of the sensor"""
+        return 'mdi:calendar'
+    @property
+    def device_state_attributes(self):
+        return {'Thông tin': self._description}
+    @Throttle(SCAN_INTERVAL)
+    def update(self):
+        self._state = solar2lunar(self._delimiter)[5]
+class lunar_calendar_good_hour_tomorrow(Entity):
+    def __init__(self, delimiter):
+        self._name = 'Giờ tốt ngày mai'
+        self._state = None
+        self._delimiter = delimiter
+        self._description = 'Âm lịch @ Hồ Ngọc Đức'
+        self.update()
+    @property
+    def name(self):
+        return self._name
+    @property
+    def state(self):
+        return self._state
+    @property
+    def icon(self):
+        """Return the icon of the sensor"""
+        return 'mdi:calendar'
+    @property
+    def device_state_attributes(self):
+        return {'Thông tin': self._description}
+    @Throttle(SCAN_INTERVAL)
+    def update(self):
+        self._state = solar2lunar(self._delimiter)[6]
+        
+class lunar_calendar_bad_hour_tomorrow(Entity):
+    def __init__(self, delimiter):
+        self._name = 'Giờ xấu ngày mai'
+        self._state = None
+        self._delimiter = delimiter
+        self._description = 'Âm lịch @ Hồ Ngọc Đức'
+        self.update()
+    @property
+    def name(self):
+        return self._name
+    @property
+    def state(self):
+        return self._state
+    @property
+    def icon(self):
+        """Return the icon of the sensor"""
+        return 'mdi:calendar'
+    @property
+    def device_state_attributes(self):
+        return {'Thông tin': self._description}
+    @Throttle(SCAN_INTERVAL)
+    def update(self):
+        self._state = solar2lunar(self._delimiter)[7]
+class lunar_calendar_nextdate(Entity):
+
+    def __init__(self, delimiter):
+        self._name = 'Âm lịch ngày mai'
+        self._state = None
+        self._delimiter = delimiter
+        self._description = 'Âm lịch @ Hồ Ngọc Đức'
+        self.update()
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return self._state
+		
+    @property
+    def icon(self):
+        """Return the icon of the sensor"""
+        return 'mdi:calendar'
+		
+    @property
+    def device_state_attributes(self):
+        return {'Thông tin': self._description}
+
+    @Throttle(SCAN_INTERVAL)
+    def update(self):
+        self._state = solar2lunar(self._delimiter)[2]
+        
+class lunar_calendar_nextday(Entity):
+
+    def __init__(self, delimiter):
+        self._name = 'Ngày âm ngày mai'
+        self._state = None
+        self._delimiter = delimiter
+        self._description = 'Âm lịch @ Hồ Ngọc Đức'
+        self.update()
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return self._state
+		
+    @property
+    def icon(self):
+        """Return the icon of the sensor"""
+        return 'mdi:calendar'
+		
+    @property
+    def device_state_attributes(self):
+        return {'Thông tin': self._description}
+
+    @Throttle(SCAN_INTERVAL)
+    def update(self):
+        self._state = solar2lunar(self._delimiter)[3]
+        
+
+
+        
+def today():
+    a = 'Hôm nay'
+    ngay = datetime.date.today()
+    yy,mm, dd = ngay.year, ngay.month, ngay.day 
+    amlich = [a,yy,mm,dd]
+    return amlich,ngay
+def tomorrow():
+    a = 'Ngày mai'
+    ngay = datetime.date.today() + timedelta(1)
+    yy,mm, dd = ngay.year, ngay.month, ngay.day 
+    amlich = [a,yy,mm,dd]
+    return amlich,ngay
+
+def solar2lunar(_delimiter):
+    today = datetime.date.today()
+    yy =  today.year
+    mm = today.month
+    dd = today.day
+    tmr = today + datetime.timedelta(1)
+    yy_tmr = tmr.year
+    mm_tmr = tmr.month
+    dd_tmr = tmr.day
+    lunar_date = S2L(dd, mm, yy)
+    lunar_date_tmr = S2L(dd_tmr, mm_tmr, yy_tmr)
+    ngay_am,thang_am,nam_am,nam_nhuan,good_h,bad_h= lunar_date
+    ngay_am_sau,thang_am_sau,nam_am_sau,nam_nhuan,good_h_s,bad_h_s = lunar_date_tmr
+    mung = 'mùng ' if ngay_am < 11 else ''
+    special = 'rằm ' if ngay_am == 15 else ''
+    ngay_am_text = ' '.join([mung,special,str(ngay_am)])
+    mung_sau = 'mùng ' if ngay_am_sau < 11 else ''
+    special_sau = 'rằm ' if ngay_am_sau == 15 else ''
+    ngay_am_text_sau = ' '.join([mung_sau,special_sau,str(ngay_am_sau)])
+    counter = ''
+    if ngay_am < 14:
+        days_left = 15 - ngay_am
+        counter =' '.join([', còn',str(days_left),' ngày nữa thì đến rằm'])
+    elif ngay_am == 14:
+        counter ='ngày mai rằm nhé'
+    else:
+        if thang_am < 12:
+            nam_a = nam_am
+            thang_sau = thang_am + 1
+        else:
+            thang_sau = 1
+            nam_a = nam_am + 1
+            nd,td,nmd = L2S(28,12,yy,nam_nhuan)
+            daa = '-'.join([str(nd),str(td),str(nmd)])
+            a2dnew = datetime.datetime.strptime(daa, '%d-%m-%Y').date()
+            ngaytet, thangtet, namtet= a2dnew.day, a2dnew.month, a2dnew.year
+            nammoi = S2L(ngaytet,thangtet,namtet)
+            nam_nhuan = nammoi[3]
+        nd,td,nmd  = L2S(1,thang_sau,nam_a,nam_nhuan)
+        daa = '-'.join([str(nd),str(td),str(nmd)])
+        a2dnew = datetime.datetime.strptime(daa, '%d-%m-%Y').date()
+        ngay_mai = tomorrow()[1]
+        delta = a2dnew - ngay_mai
+        days_left = delta.days
+        counter =' '.join([', còn',str(days_left),'ngày nữa thì đến mùng 1'])
+    list_thang = ["tháng Giêng","tháng Hai","tháng Ba","tháng Tư","tháng Năm","tháng Sáu","tháng Bảy","tháng Tám","tháng Chín","tháng Mười","tháng Mười một","tháng Chạp"]
+    thang_nay_text = list_thang[thang_am-1]
+    thang_sau_text = list_thang[thang_am_sau-1]
+    can = ['Canh ', 'Tân ', 'Nhâm ', 'Quý ', 'Giáp ', 'Ất ', 'Bính ', 'Đinh ','Mậu ','Kỷ ']
+    chi = ['Thân', 'Dậu', 'Tuất', 'Hợi','Tí','Sửu','Dần', 'Mão', 'Thìn', 'Tị', 'Ngọ', "Mùi"]
+    vitri_can = nam_am % 10
+    vitri_chi = nam_am % 12
+    vitri_can_sau = nam_am_sau % 10
+    vitri_chi_sau = nam_am_sau % 12
+    lunar_text = ' '.join(['Ngày',ngay_am_text,thang_nay_text,'năm',can[vitri_can],chi[vitri_chi]])
+    lunar_date = str(ngay_am) + "/" + str(thang_am) + "/" + str(nam_am)
+    lunar_text_sau = ' '.join(['Ngày',ngay_am_text_sau,thang_sau_text,'năm',can[vitri_can_sau],chi[vitri_chi_sau],str(nam_am_sau)])
+    lunar_date_sau = str(ngay_am_sau) + "/" + str(thang_am_sau) + "/" + str(nam_am_sau)
+    return lunar_date, lunar_text, lunar_date_sau, lunar_text_sau,good_h,bad_h,good_h_s,bad_h_s #Hiện chữ
+
+
+''' Thuật toán tính âm lịch
+(c) 2006 Ho Ngoc Duc.
+Astronomical algorithms
+from the book "Astronomical Algorithms" by Jean Meeus, 1998
+link: https://www.informatik.uni-leipzig.de/~duc/amlich/calrules.html
+'''
+def jdFromDate(dd, mm, yy):
+    '''def jdFromDate(dd, mm, yy): Compute the (integral) Julian day number of day dd/mm/yyyy, i.e., the number of days between 1/1/4713 BC (Julian calendar) and dd/mm/yyyy.'''
+    a = int((14 - mm) / 12.)
+    y = yy + 4800 - a
+    m = mm + 12 * a - 3
+    jd = dd + int((153 * m + 2) / 5.) + 365 * y + int(y / 4.) - int(y / 100.) + int(y / 400.) - 32045
+    if (jd < 2299161):
+        jd = dd + int((153 * m + 2) / 5.) + 365 * y + int(y / 4.) - 32083
+    return jd
+
+
+def jdToDate(jd):
+    '''def jdToDate(jd): Convert a Julian day number to day/month/year. jd is an integer.'''
+    if (jd > 2299160):
+        a = jd + 32044
+        b = int((4 * a + 3) / 146097.)
+        c = a - int((b * 146097) / 4.)
+    else:
+        b = 0
+        c = jd + 32082
+    d = int((4 * c + 3) / 1461.)
+    e = c - int((1461 * d) / 4.)
+    m = int((5 * e + 2) / 153.)
+    day = e - int((153 * m + 2) / 5.) + 1
+    month = m + 3 - 12 * int(m / 10.)
+    year = b * 100 + d - 4800 + int(m / 10.)
+    return [day, month, year]
+
+
+def NewMoon(k):
+    '''def NewMoon(k): Compute the time of the k-th new moon after the new moon of 1/1/1900 13:52 UCT (measured as the number of days since 1/1/4713 BC noon UCT, e.g., 2451545.125 is 1/1/2000 15:00 UTC. Returns a floating number, e.g., 2415079.9758617813 for k=2 or 2414961.935157746 for k=-2.'''
+    T = k / 1236.85
+    T2 = T * T
+    T3 = T2 * T
+    dr = math.pi / 180.
+    Jd1 = 2415020.75933 + 29.53058868 * k + 0.0001178 * T2 - 0.000000155 * T3
+    Jd1 = Jd1 + 0.00033 * math.sin((166.56 + 132.87 * T - 0.009173 * T2) * dr)
+    M = 359.2242 + 29.10535608 * k - 0.0000333 * T2 - 0.00000347 * T3
+    Mpr = 306.0253 + 385.81691806 * k + 0.0107306 * T2 + 0.00001236 * T3
+    F = 21.2964 + 390.67050646 * k - 0.0016528 * T2 - 0.00000239 * T3
+    C1 = (0.1734 - 0.000393 * T) * math.sin(M * dr) + 0.0021 * math.sin(2 * dr * M)
+    C1 = C1 - 0.4068 * math.sin(Mpr * dr) + 0.0161 * math.sin(dr * 2 * Mpr)
+    C1 = C1 - 0.0004 * math.sin(dr * 3 * Mpr)
+    C1 = C1 + 0.0104 * math.sin(dr * 2 * F) - 0.0051 * math.sin(dr * (M + Mpr))
+    C1 = C1 - 0.0074 * math.sin(dr * (M - Mpr)) + 0.0004 * math.sin(dr * (2 * F + M))
+    C1 = C1 - 0.0004 * math.sin(dr * (2 * F - M)) - 0.0006 * math.sin(dr * (2 * F + Mpr))
+    C1 = C1 + 0.0010 * math.sin(dr * (2 * F - Mpr)) + 0.0005 * math.sin(dr * (2 * Mpr + M))
+    if (T < -11):
+        deltat = 0.001 + 0.000839 * T + 0.0002261 * T2 - 0.00000845 * T3 - 0.000000081 * T * T3
+    else:
+        deltat = -0.000278 + 0.000265 * T + 0.000262 * T2
+    JdNew = Jd1 + C1 - deltat
+    return JdNew
+
+
+def SunLongitude(jdn):
+    '''def SunLongitude(jdn): Compute the longitude of the sun at any time. Parameter: floating number jdn, the number of days since 1/1/4713 BC noon.'''
+    T = (jdn - 2451545.0) / 36525.
+    T2 = T * T
+    dr = math.pi / 180.
+    M = 357.52910 + 35999.05030 * T - 0.0001559 * T2 - 0.00000048 * T * T2
+    L0 = 280.46645 + 36000.76983 * T + 0.0003032 * T2
+    DL = (1.914600 - 0.004817 * T - 0.000014 * T2) * math.sin(dr * M)
+    DL += (0.019993 - 0.000101 * T) * math.sin(dr * 2 * M) + 0.000290 * math.sin(dr * 3 * M)
+    L = L0 + DL
+    L = L * dr
+    L = L - math.pi * 2 * (int(L / (math.pi * 2)))
+    return L
+
+
+def getSunLongitude(dayNumber, timeZone):
+    '''def getSunLongitude(dayNumber, timeZone):  Compute sun position at midnight of the day with the given Julian day number. The time zone if the time difference between local time and UTC: 7.0 for UTC+7:00. The function returns a number between 0 and 11.  From the day after March equinox and the 1st major term after March equinox, 0 is returned. After that, return 1, 2, 3 ...'''
+    return int(SunLongitude(dayNumber - 0.5 - timeZone / 24.) / math.pi * 6)
+
+
+def getNewMoonDay(k, timeZone):
+    '''def getNewMoonDay(k, timeZone): Compute the day of the k-th new moon in the given time zone. The time zone if the time difference between local time and UTC: 7.0 for UTC+7:00.'''
+    return int(NewMoon(k) + 0.5 + timeZone / 24.)
+
+
+def getLunarMonth11(yy, timeZone):
+    '''def getLunarMonth11(yy, timeZone):  Find the day that starts the luner month 11of the given year for the given time zone.'''
+    off = jdFromDate(31, 12, yy) - 2415021.
+    k = int(off / 29.530588853)
+    nm = getNewMoonDay(k, timeZone)
+    sunLong = getSunLongitude(nm, timeZone)
+    if (sunLong >= 9):
+        nm = getNewMoonDay(k - 1, timeZone)
+    return nm
+
+
+def getLeapMonthOffset(a11, timeZone):
+    '''def getLeapMonthOffset(a11, timeZone): Find the index of the leap month after the month starting on the day a11.'''
+    k = int((a11 - 2415021.076998695) / 29.530588853 + 0.5)
+    last = 0
+    i = 1 
+    arc = getSunLongitude(getNewMoonDay(k + i, timeZone), timeZone)
+    while True:
+        last = arc
+        i += 1
+        arc = getSunLongitude(getNewMoonDay(k + i, timeZone), timeZone)
+        if not (arc != last and i < 14):
+            break
+    return i - 1
+
+
+def S2L(dd, mm, yy, timeZone=7):
+    '''def S2L(dd, mm, yy, timeZone=7): Convert solar date dd/mm/yyyy to the corresponding lunar date.'''
+    dayNumber = jdFromDate(dd, mm, yy)
+    k = int((dayNumber - 2415021.076998695) / 29.530588853)
+    monthStart = getNewMoonDay(k + 1, timeZone)
+    if (monthStart > dayNumber):
+        monthStart = getNewMoonDay(k, timeZone)
+    a11 = getLunarMonth11(yy, timeZone)
+    b11 = a11
+    if (a11 >= monthStart):
+        lunarYear = yy
+        a11 = getLunarMonth11(yy - 1, timeZone)
+    else:
+        lunarYear = yy + 1
+        b11 = getLunarMonth11(yy + 1, timeZone)
+    lunarDay = dayNumber - monthStart + 1
+    diff = int((monthStart - a11) / 29.)
+    lunarLeap = 0
+    lunarMonth = diff + 11
+    if (b11 - a11 > 365):
+        leapMonthDiff = getLeapMonthOffset(a11, timeZone)
+        if (diff >= leapMonthDiff):
+            lunarMonth = diff + 10
+            if (diff == leapMonthDiff):
+                lunarLeap = 1
+    if (lunarMonth > 12):
+        lunarMonth = lunarMonth - 12
+    if (lunarMonth >= 11 and diff < 4):
+        lunarYear -= 1
+    zodiac_hour_index = (lunarDay // 2) % 12
+    good_hours = [
+        'Tý', 'Sửu', 'Dần', 'Mão', 'Thìn', 'Tỵ',
+        'Ngọ', 'Mùi', 'Thân', 'Dậu', 'Tuất', 'Hợi'
+    ]
+    bad_hours = [
+        'Sửu', 'Dần', 'Tỵ', 'Ngọ', 'Mùi',
+        'Thân', 'Tuất', 'Dậu', 'Hợi', 'Tý', 'Mão', 'Thìn'
+    ]
+    hour_time_mapping = {
+        'Tý': '23h-1h',
+        'Sửu': '1h-3h',
+        'Dần': '3h-5h',
+        'Mão': '5h-7h',
+        'Thìn': '7h-9h',
+        'Tỵ': '9h-11h',
+        'Ngọ': '11h-13h',
+        'Mùi': '13h-15h',
+        'Thân': '15h-17h',
+        'Dậu': '17h-19h',
+        'Tuất': '19h-21h',
+        'Hợi': '21h-23h'
+    }
+    good_hour = good_hours[zodiac_hour_index]
+    good_hour_time = hour_time_mapping[good_hour]
+    good_hour_print = good_hour + ' (' + good_hour_time+') '
+    bad_hour = bad_hours[zodiac_hour_index]
+    bad_hour_time = hour_time_mapping[bad_hour]
+    bad_hour_print = bad_hour + ' (' + bad_hour_time+') '
+
+    return [lunarDay, lunarMonth, lunarYear, lunarLeap, good_hour_print, bad_hour_print]
+
+def L2S(lunarD, lunarM, lunarY, lunarLeap, tZ=7):
+    '''def L2S(lunarD, lunarM, lunarY, lunarLeap, tZ = 7): Convert a lunar date to the corresponding solar date.'''
+    if (lunarM < 11):
+        a11 = getLunarMonth11(lunarY - 1, tZ)
+        b11 = getLunarMonth11(lunarY, tZ)
+    else:
+        a11 = getLunarMonth11(lunarY, tZ)
+        b11 = getLunarMonth11(lunarY + 1, tZ)
+    k = int(0.5 + (a11 - 2415021.076998695) / 29.530588853)
+    off = lunarM - 11
+    if (off < 0):
+        off += 12
+    if (b11 - a11 > 365):
+        leapOff = getLeapMonthOffset(a11, tZ)
+        leapM = leapOff - 2
+        if (leapM < 0):
+            leapM += 12
+        if (lunarLeap != 0 and lunarM != leapM):
+            return [0, 0, 0]
+        elif (lunarLeap != 0 or off >= leapOff):
+            off += 1
+    monthStart = getNewMoonDay(k + off, tZ)
+    return jdToDate(monthStart + lunarD - 1)
+
+
+''' end function calendar '''    
